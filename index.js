7fc192571fddc995b2657cf1715ae104d3e551a4
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-16 03:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var cart = [];
 
 function getCart(item) {
-let newObj = {
-  itemName: item,
-  itemPrice : Math.floor(Math.random() * (100 - 1 + 1)) + 1
 
-}
-cart.push(newObj)
  return cart;
 }
 
