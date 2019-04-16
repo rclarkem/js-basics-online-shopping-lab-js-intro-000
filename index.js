dd777ec5969a90a3da38e35b64971af5a1be1661
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-16 05:35:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
 for(let i = 0; i < cart.length; i++){
 	if(cart[i]["itemName"] === item){
 		console.log(i)
-		cart.splice(i,1,)
+		cart.splice(i,1)
   } else if(cart[i]['itemName'] !== item){
     // if(!cart.indexOf(item)){
      return "That item is not in your cart."
