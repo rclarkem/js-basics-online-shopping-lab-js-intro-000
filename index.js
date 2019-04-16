c4d7d3db8d561c885ca6428fbef1ca5c87a23efc
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-16 03:27:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ function setCart(c) {
 function addToCart(item) {
  let newObj = {
    itemName: item,
-   itemPrice : Math.floor(Math.random() * (100 - 1 + 1)) + 1
+   itemPrice : "$" + Math.floor(Math.random() * (100 - 1 + 1)) + 1
  };
  cart.push(newObj)
 return `${newObj["itemName"]} has been added to your cart.`
 }
 
 function viewCart() {
+  if(cart.length < 1){
+    return "Your shopping cart is empty."
+  }
 for(let i = 0; i < cart.length; i++){
   return `In your cart, you have ${cart[i]["itemName"]} at ${cart[i]["itemPrice"]}. `
 }
