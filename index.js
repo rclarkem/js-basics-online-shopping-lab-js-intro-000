f6307de54262b161f03cb725a28f7f7a06ac4837
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-16 05:39:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,19 +47,20 @@ return sum
 }
 
 function removeFromCart(item) {
-for(let i = 0; i < cart.length; i++){
-	if(cart[i]["itemName"] === item){
-		console.log(i)
-		cart.splice(i,1)
-  } else if(cart[i]['itemName'] !== item){
-    // if(!cart.indexOf(item)){
+  function removeFromCart(item) {
+    if(!cart.indexOf(item)){
      return "That item is not in your cart."
-    // }
-  }
+  	}
+  for(let i = 0; i < cart.length; i++){
+  	if(cart[i]["itemName"] === item){
+  		console.log(cart[i])
+  		cart.splice(i,1)
 
-}
-return cart
-}
+  	}
+
+  }
+  return cart
+  }
 
 function placeOrder(cardNumber) {
   // write your code here
